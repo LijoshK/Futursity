cc3e389c47e6ca9ad57c3c36710a0da276d49c3b
LijoshK <lijoshk457@gmail.com>
2023-02-23 13:14:30 +0530

    commit

--- lib/view/course_pg.dart
+++ lib/view/course_pg.dart
@@ -17,6 +17,7 @@ class _Course_pgState extends State<Course_pg> {
     double x = size.height * 2;
     double y = size.width / 2 - 20;
     return Scaffold(
+
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 1,
@@ -122,94 +123,92 @@ class _Course_pgState extends State<Course_pg> {
               ],
             ),
           ),
-          Expanded(
-            child: FutureBuilder(
-                future: Api.getCategories(),
-                builder: (context, snapshot) {
-                  return snapshot.connectionState == ConnectionState.waiting
-                      ? Center(
-                          child: CircularProgressIndicator(),
-                        )
-                      : GridView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemCount: Api.categories!.length,
-                           physics: NeverScrollableScrollPhysics(),
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            Categories category = Api.categories![index];
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                height: x = size.height * 0.2,
-                                width: y = size.width * 0.4,
-                                decoration: BoxDecoration(
-                                    color: index % 3 == 0
-                                        ? Colors.blue[900]
-                                        : index % 3 == 1
-                                            ? Colors.red
-                                            : Colors.teal[400],
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(20)),
-                                    boxShadow: [
-                                      BoxShadow(
-                                          color: Colors.grey,
-                                          spreadRadius: 2,
-                                          blurStyle: BlurStyle.outer,
-                                          blurRadius: 8)
-                                    ]),
-                                child: Column(
-                                  children: [
-                                    Container(
-                                      height: x / 1.6,
-                                      decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.only(
-                                              topLeft: Radius.circular(20),
-                                              topRight: Radius.circular(20)),
-                                          image: DecorationImage(
-                                              fit: BoxFit.fill,
-                                              image: NetworkImage(
-                                                category.thumbnail,
-                                              ))),
-                                    ),
-                                    Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                        child: Stack(
-                                          children: [
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.all(8.0),
-                                              child: Text(
-                                                category.name,
-                                                style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              ),
+          FutureBuilder(
+              future: Api.getCategories(),
+              builder: (context, snapshot) {
+                return snapshot.connectionState == ConnectionState.waiting
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : GridView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: Api.categories!.length,
+                         physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          Categories category = Api.categories![index];
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: x = size.height * 0.2,
+                              width: y = size.width * 0.4,
+                              decoration: BoxDecoration(
+                                  color: index % 3 == 0
+                                      ? Colors.blue[900]
+                                      : index % 3 == 1
+                                          ? Colors.red
+                                          : Colors.teal[400],
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(20)),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.grey,
+                                        spreadRadius: 2,
+                                        blurStyle: BlurStyle.outer,
+                                        blurRadius: 8)
+                                  ]),
+                              child: Column(
+                                children: [
+                                  Container(
+                                    height: x / 1.6,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(20),
+                                            topRight: Radius.circular(20)),
+                                        image: DecorationImage(
+                                            fit: BoxFit.fill,
+                                            image: NetworkImage(
+                                              category.thumbnail,
+                                            ))),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      child: Stack(
+                                        children: [
+                                          Padding(
+                                            padding:
+                                                const EdgeInsets.all(8.0),
+                                            child: Text(
+                                              category.name,
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight:
+                                                      FontWeight.bold),
                                             ),
-                                            Positioned(
-                                                left: 2,
-                                                top: 45,
-                                                child: Text(
-                                                    "${category.numberOfCourses} Courses",
-                                                    style: TextStyle(
-                                                      color: Colors.white,
-                                                    ))),
-                                          ],
-                                        ),
+                                          ),
+                                          Positioned(
+                                              left: 2,
+                                              top: 45,
+                                              child: Text(
+                                                  "${category.numberOfCourses} Courses",
+                                                  style: TextStyle(
+                                                    color: Colors.white,
+                                                  ))),
+                                        ],
                                       ),
                                     ),
-                                  ],
-                                ),
+                                  ),
+                                ],
                               ),
-                            );
-                          },
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: 2),
-                        );
-                }),
-          ),
+                            ),
+                          );
+                        },
+                        gridDelegate:
+                            SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2),
+                      );
+              }),
         ],
       ),
     );
